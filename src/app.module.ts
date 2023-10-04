b5bb7a02f6fc4af0095b04235bbb10537c7074a4
Camilo Molano <ecamilo.molano@gmail.com>
2023-10-04 18:50:28 -0500

    feat: configure TypeOrmModule with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { PurchasesModule } from './purchases/purchases.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ProductsModule, PurchasesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity.js'],
+      synchronize: true,
+    }),
+    UsersModule,
+    ProductsModule,
+    PurchasesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
