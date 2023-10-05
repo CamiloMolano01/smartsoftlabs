2e0657a0a3987e6d6a72ff8e858e46f6db1c33c4
Camilo Molano <ecamilo.molano@gmail.com>
2023-10-05 09:24:03 -0500

    feat: add class validator

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
   readonly category: string;
+
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsPositive()
   readonly quantity: number;
 }
